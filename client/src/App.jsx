72d92fe28a76afb3897eab4d6eff093fac1fc3f8
RONALDMASLOG2024-2025 <rmaslog_230000000654@uic.edu.ph>
2025-04-14 03:32:36 +0800

    Second Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  
+
+  const [message, setMessage] = useState('')
+
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_API_URL}/hello`).then((res)=> setMessage(res.data.message)).catch((err)=>{
+      console.error('Axios Error:', err)
+      setMessage('Failed to fetch message')
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>{message}</h1>
+      <p>HELLO</p>
+    </>
+  )
+}
+
+export default App
