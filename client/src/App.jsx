47abd5c13f5c2700a5e9c00362e1de41ebe92190
RONALDMASLOG2024-2025 <rmaslog_230000000654@uic.edu.ph>
2025-04-14 12:00:34 +0800

    Updating with the database connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,39 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  
+  const [message, setMessage] = useState("");
+  const [dbmessage, setDbmessage] = useState("");
 
-  const [message, setMessage] = useState('')
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/api/hello`)
+      .then((res) => setMessage(res.data.message))
+      .catch((err) => {
+        console.error("Axios Error:", err);
+        setMessage("Failed to fetch message");
+      });
 
-  useEffect(()=>{
-    axios.get(`${import.meta.env.VITE_API_URL}/hello`).then((res)=> setMessage(res.data.message)).catch((err)=>{
-      console.error('Axios Error:', err)
-      setMessage('Failed to fetch message')
-    })
-  }, [])
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/api/database`)
+      .then((res) => setDbmessage(res.data.dbmessage))
+      .catch((err) => {
+        console.error("Axios Error:", err);
+        setMessage("Failed to fetch database message");
+      });
+  }, []);
 
   return (
     <>
       <h1>{message}</h1>
       <p>HELLO Ron</p>
+
+      <h4>{dbmessage}</h4>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,27 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-
+var dbmsg;
 
 //My Database connection USING MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(()=>console.log("Connected to MongoDB Atlas")).catch((err) => console.error('MongoDB connection error:', err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+    dbmsg = "Ronald Maslog your database is connected";
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    dbmsg = "Database connection lost!";
+  });
 
+app.get("/api/database", (req, res) => {
+  res.json({ database: dbmsg });
+});
 
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Welcome Ronald Maslog... Your Todo Task Today!" });
+});
 
-app.get('/api/hello', (req, res) => {
-    res.json({message: 'Welcome Ronald Maslog... Your Todo Task Today!'})
-})
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
