92980091df8e57bbfd7c65f21aa3f5704f156f42
RONALDMASLOG2024-2025 <rmaslog_230000000654@uic.edu.ph>
2025-04-18 22:12:29 +0800

    Update firebase-admin.js

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -3,15 +3,14 @@ require("dotenv").config();
 
 // const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
-let serviceAccount;
 
-if (process.env.FIREBASE_CONFIG) {
-  // On Render or production
-  serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-} else {
+
+
   // Local development fallback
-  serviceAccount = require("./doodledo-d7c4e-firebase-adminsdk-fbsvc-9272b64d8e.json");
-}
+  const serviceAccount = require("/etc/secrets/doodledo-d7c4e-firebase-adminsdk-fbsvc-9272b64d8e.json");
+
+  // const serviceAccount = require("./doodledo-d7c4e-firebase-adminsdk-fbsvc-9272b64d8e.json");
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
