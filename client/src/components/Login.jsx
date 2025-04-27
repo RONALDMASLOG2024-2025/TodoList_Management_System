eec2fe192beba6a84c17caebdaf8669993390708
Ronald P. Maslog <rmaslog_230000000654@uic.edu.ph>
2025-04-27 16:05:26 +0800

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -50,7 +50,7 @@ function Login() {
           </button>
 
           <div className="absolute bottom-4 text-center text-sm opacity-50">
-            <p className="text-gray-100">Made by Maslog & Aung</p>
+            <p className="text-gray-100">Made by Maslog</p>
             <p className="text-gray-100">Year 2025</p>
           </div>
         </>
